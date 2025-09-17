fec92cc4232d6ad37c49ba45df28d8c26f904281
info843 <info@newvisionlab.net>
2025-09-17 12:05:53 +0200

    Delete api/ping.js

--- api/ping.js
+++
@@ -1,6 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  return res.status(200).json({ ok: true, path: req.url, ts: Date.now() });
-};
