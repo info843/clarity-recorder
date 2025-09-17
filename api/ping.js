ba18b6f05df2fa9bd6dde9798f31bb4805910276
info843 <info@newvisionlab.net>
2025-09-17 16:27:24 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
   if (req.method === 'OPTIONS') return res.status(204).end();
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ ok:true, where:'serverless', time: Date.now() });
+  res.status(200).json({ ok: true, where: 'serverless', ts: Date.now() });
 }
