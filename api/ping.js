6ff7ee569ec3c40d61f1dbe224e08f16175cb9fb
info843 <info@newvisionlab.net>
2025-09-17 12:12:40 +0200

    Create ping.js

---
+++ api/ping.js
@@ -0,0 +1,10 @@
+export default async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(204).end();
+  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200).json({ ok: true, where: 'serverless', time: new Date().toISOString() });
+}
