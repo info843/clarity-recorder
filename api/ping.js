502e2df8e5362f64a6478971f456c9bb77b1979e
info843 <info@newvisionlab.net>
2025-09-17 16:06:25 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,10 @@
 export default async function handler(req, res) {
-  if (req.method === 'OPTIONS') return res.status(204).end();
+  if (req.method === 'OPTIONS') return res.status(204)
+    .setHeader('Access-Control-Allow-Origin', '*')
+    .setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+    .setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    .end();
+
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ ok:true, where:'serverless', time: Date.now() });
+  return res.status(200).json({ ok: true, where: 'serverless', time: Date.now() });
 }
