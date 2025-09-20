6945c81c06c67436267968f6884dde6c67f302ec
info843 <info@newvisionlab.net>
2025-09-20 11:01:24 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,9 +1,9 @@
-// CJS, funktioniert in "Framework: Other" und allen Vercel-Setups
-module.exports = async (req, res) => {
+// ESM
+export default async function handler(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.status(200).end(JSON.stringify({
     ok: true,
     now: new Date().toISOString(),
     method: req.method
   }));
-};
+}
