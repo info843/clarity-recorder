5889308b6cf02b7796f2b87a07829ed0e83da914
info843 <info@newvisionlab.net>
2026-07-26 14:35:50 +0200

    Update interview-module.js

--- modules/interview-module.js
+++ modules/interview-module.js
@@ -1,8 +1,8 @@
 // modules/interview-module.js
-// CLARITY Universal App - Interview module I1.7 v1.1.0
+// CLARITY Universal App - Interview module I1.7.2 v1.1.1
 // Lost-response recovery for committed starts and idempotent chat messages.
 
-const MODULE_VERSION='1.1.0-i1-7';
+const MODULE_VERSION='1.1.1-i1-7-2';
 const FRAME_BY_MODE=Object.freeze({
   chat:`./modules/interview-chat.html?v=${MODULE_VERSION}`,
   audio:'./modules/interview-audio-recorder.html?v=1.0.0',
